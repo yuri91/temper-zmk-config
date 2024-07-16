cbae2226ec297aa5f41b11422c48c757426a708f
Yuri Iozzelli <y.iozzelli@gmail.com>
2024-07-16 15:11:57 +0200

    add conf for firmware reset

--- flake.nix
+++ flake.nix
@@ -10,32 +10,35 @@
 
   outputs = { self, nixpkgs, zmk-nix }: let
     forAllSystems = nixpkgs.lib.genAttrs (nixpkgs.lib.attrNames zmk-nix.packages);
+    make-firmware = (system: shield: zmk-nix.legacyPackages.${system}.buildSplitKeyboard rec {
+      name = "firmware";
+
+      src = ./.;
+
+      board = "nice_nano_v2";
+      inherit shield;
+
+      extraCmakeFlags = ["-DZMK_EXTRA_MODULES=${src}"];
+
+      zephyrDepsHash = "sha256-wdIi+7Cqo+PjAn6Imq7Pao1ErSGy5WVj1ddzjz87H+8=";
+
+      meta = {
+        description = "ZMK firmware";
+        license = nixpkgs.lib.licenses.mit;
+        platforms = nixpkgs.lib.platforms.all;
+      };
+    });
   in {
     packages = forAllSystems (system: rec {
       default = firmware;
 
-      firmware = zmk-nix.legacyPackages.${system}.buildSplitKeyboard rec {
-        name = "firmware";
-
-        src = ./.;
-
-        board = "nice_nano_v2";
-        shield = "temper_%PART%";
-
-        extraCmakeFlags = ["-DZMK_EXTRA_MODULES=${src}"];
-
-        zephyrDepsHash = "sha256-wdIi+7Cqo+PjAn6Imq7Pao1ErSGy5WVj1ddzjz87H+8=";
-
-        meta = {
-          description = "ZMK firmware";
-          license = nixpkgs.lib.licenses.mit;
-          platforms = nixpkgs.lib.platforms.all;
-        };
-      };
+      firmware = make-firmware system "temper_%PART%";
+      firmware-reset = make-firmware system "settings_reset";
 
       keymap-drawer = nixpkgs.legacyPackages.${system}.callPackage ./nix/keymap-drawer.nix {};
 
       flash = zmk-nix.packages.${system}.flash.override { inherit firmware; };
+      flash-reset = zmk-nix.packages.${system}.flash.override { firmware = firmware-reset; };
       update = zmk-nix.packages.${system}.update;
     });
 
